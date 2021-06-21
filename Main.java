160e4c329a0e353afc6370c085281bdbe5897b3b
krisstallino <krisstallino@gmail.com>
2021-06-21 13:48:52 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_cost = 5600;
+        int bonus_mile = 20;
+        int extra_mile = ticket_cost / bonus_mile;
+        System.out.println("You have " + extra_mile + " bonus miles");
+    }
+}
